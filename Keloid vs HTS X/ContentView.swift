2a523a108559e2587b9c4c0fef83c2941e61ffee
pmg61232 <111905508+pmg61232@users.noreply.github.com>
2022-09-05 18:53:44 -0500

    Initial Commit

---
+++ Keloid vs HTS X/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Keloid vs HTS X
+//
+//  Created by Peter Granger on 9/5/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Keloid vs HTS X/Keloid_vs_HTS_XApp.swift
@@ -0,0 +1,17 @@
+//
+//  Keloid_vs_HTS_XApp.swift
+//  Keloid vs HTS X
+//
+//  Created by Peter Granger on 9/5/22.
+//
+
+import SwiftUI
+
+@main
+struct Keloid_vs_HTS_XApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
